6a124fb7e47ed0a6b33305cf2956f5f34db084b1
Joseph Levenson <josephlevenson@Dev-JLevenson.percussion.local>
2015-01-13 14:10:24 -0500

    Changed the background and changed some styling

--- scripts/script.js
+++ scripts/script.js
@@ -13,7 +13,8 @@ $.getJSON('scripts/inventory.json', function(response) {
 		else {
 			wine_panel.className = "panel panel-danger";
 		}
-		
+
+				
 		document.getElementById("wine" + i).appendChild(wine_panel);
 
 		var wine_title_container = document.createElement("div");
@@ -23,16 +24,30 @@ $.getJSON('scripts/inventory.json', function(response) {
 		document.getElementById("wine" + i).getElementsByClassName("panel")[0].appendChild(wine_title_container);
 
 		for (var attribute in response[i]) {
-			if (attribute == "Color") continue;
+			if (attribute == "Color" || attribute == "Name") continue;
 			var container = document.createElement("div");
 			container.className = "list-group-item";
 			var heading = document.createElement("h4");
 			heading.className = "list-group-item-heading";
-			text = document.createTextNode(attribute);
-			heading.appendChild(text);
+			heading_text = document.createTextNode(attribute);
+			heading.appendChild(heading_text);
 			text = document.createTextNode(response[i][attribute]);
 			container.appendChild(heading);
-			container.appendChild(text);
+			if (attribute == "Characteristics") {
+				for (var characteristic in response[i][attribute]) {
+					var val = response[i][attribute][characteristic];
+					var bar_container = document.createElement("div");
+					bar_container.className = "progress";
+					var bar = document.createElement("div");
+					bar.className = "progress-bar progress-bar-info";
+					bar.style.width = String(val) + "%";
+					var bar_text = document.createTextNode(characteristic);
+					bar.appendChild(bar_text);
+					bar_container.appendChild(bar);
+					container.appendChild(bar_container);
+				}
+			}
+			else container.appendChild(text);
 			document.getElementById("wine" + i).getElementsByClassName("panel")[0].appendChild(container);
 		}
 	}
